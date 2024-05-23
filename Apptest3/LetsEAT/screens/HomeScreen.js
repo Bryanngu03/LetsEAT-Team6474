aa69c774857a1ce1813c1fea4b4a8b300e46e343
Lo Liang-Cheng <156636679+loliangcheng@users.noreply.github.com>
2024-05-23 15:00:03 +0800

    part 4 bug fix & UI update
    -reduce header space
    -fixed icon not showing properly

--- Apptest3/LetsEAT/screens/HomeScreen.js
+++ Apptest3/LetsEAT/screens/HomeScreen.js
@@ -1,41 +1,39 @@
-// screens/HomeScreen.js
 import React from "react";
 import { View, Text, StyleSheet, Image, FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import moment from "moment";
 
-// temporary data until we pull from Firebase
 const posts = [
     {
         id: "1",
-        name: "Joe McKay",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        timestamp: 1569109273726,
+        name: "Sarah Williams",
+        text: "Flavourful nasi lemak I had at Madam Kuam!",
+        timestamp: 1716445995000, //timestamp in milliseconds
         avatar: require("../assets/tempAvatar.jpg"),
         image: require("../assets/tempImage1.jpg")
     },
     {
         id: "2",
-        name: "Karyn Kim",
-        text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        timestamp: 1569109273726,
-        avatar: require("../assets/tempAvatar.jpg"),
+        name: "Cody Lim",
+        text: "Supurb chicken rice! Got discount now, while stocks last~",
+        timestamp: 1716442393000, //timestamp in milliseconds
+        avatar: require("../assets/tempAvatar2.jpg"),
         image: require("../assets/tempImage2.jpg")
     },
     {
         id: "3",
-        name: "Emerson Parsons",
-        text: "Amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant.",
-        timestamp: 1569109273726,
-        avatar: require("../assets/tempAvatar.jpg"),
+        name: "Chen Huihui",
+        text: "I love big macs!",
+        timestamp: 1716441793000, //timestamp in milliseconds
+        avatar: require("../assets/tempAvatar3.jpg"),
         image: require("../assets/tempImage3.jpg")
     },
     {
         id: "4",
-        name: "Kathie Malone",
-        text: "At varius vel pharetra vel turpis nunc eget lorem. Lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor. Adipiscing tristique risus nec feugiat in fermentum.",
-        timestamp: 1569109273726,
-        avatar: require("../assets/tempAvatar.jpg"),
+        name: "Aditi",
+        text: "Delicious homemade pasta, DM for recipe!",
+        timestamp: 1569109273726, //timestamp in milliseconds
+        avatar: require("../assets/tempAvatar4.jpg"),
         image: require("../assets/tempImage4.jpg")
     }
 ];
@@ -52,13 +50,13 @@ export default class HomeScreen extends React.Component {
                             <Text style={styles.timestamp}>{moment(post.timestamp).fromNow()}</Text>
                         </View>
 
-                        <Ionicons name="ios-more" size={24} color="#73788B" />
+                        <Ionicons name="ellipsis-horizontal" size={24} color="#73788B" />
                     </View>
                     <Text style={styles.post}>{post.text}</Text>
                     <Image source={post.image} style={styles.postImage} resizeMode="cover" />
                     <View style={{ flexDirection: "row" }}>
-                        <Ionicons name="ios-heart-empty" size={24} color="#73788B" style={{ marginRight: 16 }} />
-                        <Ionicons name="ios-chatboxes" size={24} color="#73788B" />
+                        <Ionicons name="heart-outline" size={24} color="#73788B" style={{ marginRight: 16 }} />
+                        <Ionicons name="chatbox-outline" size={24} color="#73788B" />
                     </View>
                 </View>
             </View>
@@ -78,7 +76,7 @@ export default class HomeScreen extends React.Component {
                     renderItem={({ item }) => this.renderPost(item)}
                     keyExtractor={item => item.id}
                     showsVerticalScrollIndicator={false}
-                ></FlatList>
+                />
             </View>
         );
     }
@@ -90,7 +88,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#EBECF4"
     },
     header: {
-        paddingTop: 64,
+        paddingTop: 16, //adjusted from 64
         paddingBottom: 16,
         backgroundColor: "#FFF",
         alignItems: "center",
